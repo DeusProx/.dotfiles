2bc5a4a5b4752e8185221bfa8e44134d57b5083c
Gordon Lawrenz <gordon@lawrenz.email>
2023-04-01 22:45:17 +0200

    Add basic neovim config

---
+++ .config/nvim/lua/deusprox/options.lua
@@ -0,0 +1,21 @@
+-- See :help vim.o
+-- Apply reasonable defaults
+
+-- You shall not create strange files
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- You shall show line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- You shall not wrap lines
+vim.opt.wrap = false
+
+-- You shall handle tab as two spaces (and be smart about it)
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
