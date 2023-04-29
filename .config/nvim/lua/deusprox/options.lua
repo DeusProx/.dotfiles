75f781c032b5844ecf91c2e98cdf5593fda9576c
Gordon Lawrenz <gordon@lawrenz.email>
2023-04-29 23:49:02 +0200

    color columns in nvim so line length does not get too long

--- .config/nvim/lua/deusprox/options.lua
+++ .config/nvim/lua/deusprox/options.lua
@@ -3,11 +3,15 @@
 
 -- You shall not create strange files
 vim.opt.swapfile = false
-vim.opt.backup = false
+vim.opt.backup = false                                                    
 
 -- You shall show line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+-- You shall color certain columns so length of line can be kept short
+vim.opt.colorcolumn = '80,120,140,160'
+vim.api.nvim_set_hl(0, 'colorcolumn', { ctermbg=0 })
 
 -- You shall not wrap lines
 vim.opt.wrap = false
