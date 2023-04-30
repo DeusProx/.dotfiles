486115f5a365c205a0b172051b1f7740808686d9
Gordon Lawrenz <gordon@lawrenz.email>
2023-04-30 17:00:09 +0200

    add treesitter with support for some stuff I need

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,6 +28,85 @@ require('lazy').setup({
     end,
   },
   { 'lukas-reineke/indent-blankline.nvim' },
+
+  -- tree-sitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      -- for syntax-highlight, instead of regular expressions, use tree-sitter:
+      highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      ensure_installed = {
+        -- scripts
+        'bash',
+        'python',
+        'lua',
+
+        -- document
+        'latex',
+        'bibtex',
+        'markdown',
+        'markdown_inline',
+
+        -- web
+        'typescript',
+        'tsx',
+        'javascript',
+        'html',
+        'css',
+        'scss',
+
+        -- rust
+        'rust',
+        'ron',
+
+        -- oldschool
+        'c',
+        'c_sharp',
+        'cpp',
+
+        -- rendering
+        'glsl',
+        'hlsl',
+        'wgsl',
+
+        -- build
+        'make',
+        'cmake',
+        'dockerfile',
+
+        -- tooling
+        -- 'diff',
+        -- 'comment',
+        -- 'git_config',
+        -- 'git_rebase',
+        -- 'gitattributes',
+        -- 'gitcommit',
+        -- 'gitignore',
+
+        -- data
+        'json',
+        'yaml',
+        'proto',
+
+        -- misc
+        'regex',
+        'sql',
+      }
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end,
+
+  },
 })
 
 require('indent_blankline').setup({
