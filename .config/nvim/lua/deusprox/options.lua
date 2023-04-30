b0d7735cead6711a900cb66644df4aae6f0ca1dd
Gordon Lawrenz <gordon@lawrenz.email>
2023-04-30 14:21:04 +0200

    make sure that true color is enabled

--- .config/nvim/lua/deusprox/options.lua
+++ .config/nvim/lua/deusprox/options.lua
@@ -5,13 +5,15 @@
 vim.opt.swapfile = false
 vim.opt.backup = false
 
+-- You shall use true 24-bit colours in the terminal!
+vim.opt.termguicolors = true
+
 -- You shall show line numbers!
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
 -- You shall color certain columns so length of line can be kept short!
 vim.opt.colorcolumn = '80,120,140,160'
-vim.api.nvim_set_hl(0, 'colorcolumn', { ctermbg=0 })
 
 -- You shall not wrap lines!
 vim.opt.wrap = false
@@ -22,3 +24,4 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+
