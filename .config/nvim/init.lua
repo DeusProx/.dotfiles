669b713425990a0fc8dac8029a3b354551b6542d
Gordon Lawrenz <gordon@lawrenz.email>
2023-04-30 13:15:47 +0200

    add lazy.vim as plugin manager for nvim and use tokyo night theme and a basic indentation plugin manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,5 +13,24 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({})
+require('lazy').setup({
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      -- vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme tokyonight-night]])
+      -- vim.cmd([[colorscheme tokyonight-storm]])
+      -- vim.cmd([[colorscheme tokyonight-day]])
+      -- vim.cmd([[colorscheme tokyonight-moon]])
+    end,
+  },
+  { 'lukas-reineke/indent-blankline.nvim' },
+})
+
+require('indent_blankline').setup({
+  space_char_blankline = ' ',
+})
 
