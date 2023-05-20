207a27e0adea672db21fbe305dcda5069e7092cc
Gordon Lawrenz <gordon@lawrenz.email>
2023-05-20 18:20:27 +0200

    Make it easier to cycle through open buffers in neovim

--- .config/nvim/lua/deusprox/remap.lua
+++ .config/nvim/lua/deusprox/remap.lua
@@ -2,3 +2,11 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('i', 'jk', '<Esc>', options)
 
+vim.keymap.set('n', '<leader>h', '<cmd>bprev<CR>')
+vim.keymap.set('n', '<leader>l', '<cmd>bnext<CR>')
+
+-- https://www.reddit.com/r/neovim/comments/10fr6gh/comment/j4zcto7/?utm_source=reddit&utm_medium=web2x&context=3
+-- vim.keymap.set('n', '<M-h>', '<cmd>bprev<CR>')
+-- vim.keymap.set('n', '<M-l>', '<cmd>bnext<CR>')
+-- vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>')
+
