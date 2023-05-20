718bf7ea65794190295d5849f8f92b9f3fafb8b9
Gordon Lawrenz <gordon@lawrenz.email>
2023-05-20 20:44:05 +0200

    keymap to show keymaps via telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,8 +2,11 @@ local builtin = require('telescope.builtin')
 
 -- choose '<leader>f' as common prefix for file commands
 
+vim.keymap.set('n', '<leader>km', builtin.keymaps, {})     -- show keymaps
+
 vim.keymap.set('n', '<leader>fu', builtin.buffers, {})     -- list buffers
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})     -- list buffers
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})  -- search fuzzy
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})   -- search git
 
