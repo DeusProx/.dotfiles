ac8c60506f0b8627516c8d07f90c074b7b3e64b5
Gordon Lawrenz <g.lawrenz@iplabs.de>
2023-06-04 17:43:19 +0200

    Add update script and add some further basics to it and the installation script

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -6,20 +6,43 @@
 #   Do not think everything will work after running this.
 #
 
-# Add apt repositories
+# package manager
 
-## Preparation
-sudo apt-get install software-properties-common
+## system
+sudo apt update
+sudo apt upgrade
 
-## Add the actual repositories
+## rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install cargo-update
+
+# preparation
+
+## add repositories
 sudo add-apt-repository ppa:neovim-ppa/unstable
 
+## update for added repositories
+sudo apt update
+
+## general tools
+sudo apt install software-properties-common
+
+# tools
+
+## tmux
+sudo apt install tmux
+
+# gitui
+cargo install gitui
+
+## Add the actual repositories
+
 ## Finally add applications from repositories
-sudo apt-get update
-sudo apt-get install neovim
+sudo apt update
+sudo apt install neovim
 
 ## neovim plugins
-sudo apt-get install ripgrep
+sudo apt install ripgrep
 sudo apt install fd-find
 npm install -g tree-sitter-cli
 npm install -g neovim
