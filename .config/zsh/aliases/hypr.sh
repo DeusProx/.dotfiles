2b91010ce8934126e1b806c027b87a7779f4dea3
Gordon Lawrenz <gordon@lawrenz.email>
2024-11-21 19:00:37 +0100

    Do not overwrite the old command_not_found_handler, but wrap it

--- .config/zsh/aliases/hypr.sh
+++ .config/zsh/aliases/hypr.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env zsh
 
+if typeset -f command_not_found_handler > /dev/null; then
+    OLD_HANDLER=$(typeset -f command_not_found_handler)
+fi
+
+command_not_found_handler() {
+  if [[ -n ${CMD_CORRECTIONS[$1]} ]]; then
+    echo -e "${YELLOW}Correcting '$1' to '${CMD_CORRECTIONS[$1]}'${RESET}"
+    "${CMD_CORRECTIONS[$1]}" "${@:2}"
+    return $EXIT_OK
+  fi
+
+  if [[ -n "$OLD_HANDLER" ]]; then
+    eval "$OLD_HANDLER" "$@"
+    return $?
+  fi
+
+  return COMMAND_NOT_FOUND
+}
+
 YELLOW="\033[33m"
 RESET="\033[0m"
 
-COMMAND_NOT_FOUND=127
+EXIT_OK=0
+EXIT_COMMAND_NOT_FOUND=127
 
 declare -A CMD_CORRECTIONS=(
   ["Hyperland"]="Hyprland"
@@ -14,14 +34,4 @@ declare -A CMD_CORRECTIONS=(
   ["hyperlock"]="hyprlock"
   ["hyperpm"]="hyprpm"
 )
-
-# Generalized command-not-found handler
-command_not_found_handler() {
-  if [[ -n ${CMD_CORRECTIONS[$1]} ]]; then
-    echo -e "${YELLOW}Correcting '$1' to '${CMD_CORRECTIONS[$1]}'${RESET}"
-    "${CMD_CORRECTIONS[$1]}" "${@:2}"
-    return
-  fi
-  return COMMAND_NOT_FOUND
-}
 
