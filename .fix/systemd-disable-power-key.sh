f03f4650919f509eb3580aa919ff1681f8f14b49
Gordon Lawrenz <gordon@lawrenz.email>
2025-11-26 19:27:27 +0100

    improve powerkey fix so longpresses are surely ignored as well

--- .fix/systemd-disable-power-key.sh
+++ .fix/systemd-disable-power-key.sh
@@ -2,11 +2,21 @@
 
 # INFO: They put the fucking Power Key of my Lenovo 16APK10 on the right side of the chassis.
 #       This leads to accidentally shutting down my system when the device sits in my lap.
+#
+# See https://www.freedesktop.org/software/systemd/man/latest/logind.conf.html
 
 sudo mkdir -p /etc/systemd/logind.conf.d
 sudo tee /etc/systemd/logind.conf.d/ignore-power-key.conf >/dev/null <<EOF
 [Login]
+HandleHibernateKey=ignore
 HandlePowerKey=ignore
+HandleRebootKey=ignore
+HandleSuspendKey=ignore
+
+HandleHibernateKeyLongPress=ignore
+HandlePowerKeyLongPress=ignore
+HandleRebootKeyLongPress=ignore
+HandleSuspendKeyLongPress=ignore
 
 EOF
 
