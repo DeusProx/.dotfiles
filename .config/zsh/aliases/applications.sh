86e1a76569bc5f427859e34f79c70b81881b5b70
Gordon Lawrenz <gordon@lawrenz.email>
2026-02-27 14:33:55 +0100

    better calculation with awk instead of echo

--- .config/zsh/aliases/applications.sh
+++ .config/zsh/aliases/applications.sh
@@ -1,8 +1,14 @@
 # you shall calculate easily
 calc() {
-  echo "$(( $@ ))"
+  awk "BEGIN {print $*}";
 }
+alias calc='noglob calc' # Necessary so * (multiplication) does not get expanded
 alias c='calc'
+
+# Just leaving this here as an example for easy calculation; has float rounding problems
+# calc() {
+#   echo $(( $@ ))
+# }
 
 # you shall use neovim!
 alias v='nvim'
