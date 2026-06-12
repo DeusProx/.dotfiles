f3dd077699ec0d22163deb77481be03d6cfa0500
Gordon Lawrenz <gordon@lawrenz.email>
2026-06-12 12:47:15 +0200

    Add alias for waybar so it automarically watches changes

--- .config/zsh/aliases/system.sh
+++ .config/zsh/aliases/system.sh
@@ -5,5 +5,7 @@ alias update-mirrors='sudo reflector --verbose -l 50 -p https --sort rate --save
 alias reload-tmux='tmux source-file ~/.config/tmux/tmux.conf'
 
 alias reload-audio='systemctl --user restart pipewire pipewire-pulse wireplumber'
+
 alias reload-waybar='pkill waybar 2>/dev/null || true; waybar >/dev/null 2>&1 &!'
+alias watch-waybar='find ~/.config/waybar -type f | entr -p zsh -ic reload-waybar'
 
