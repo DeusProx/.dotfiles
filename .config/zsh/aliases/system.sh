bacdb22c389b466d3bc33af13d48237779017058
Gordon Lawrenz <gordon@lawrenz.email>
2026-07-13 00:33:14 +0200

    Add zsh command to list binaries of a repository package

--- .config/zsh/aliases/system.sh
+++ .config/zsh/aliases/system.sh
@@ -1,7 +1,6 @@
 # open pdf files directly by their name/path
 alias -s pdf='xdg-open'
 
-alias update-mirrors='sudo reflector --verbose -l 50 -p https --sort rate --save /etc/pacman.d/mirrorlist'
 alias reload-tmux='tmux source-file ~/.config/tmux/tmux.conf'
 
 alias reload-audio='systemctl --user restart pipewire pipewire-pulse wireplumber'
@@ -10,5 +9,7 @@ alias reload-waybar='pkill waybar 2>/dev/null || true; waybar >/dev/null 2>&1 &!
 alias watch-waybar='find ~/.config/waybar -type f | entr -p zsh -ic reload-waybar'
 alias debug-waybar='env GTK_DEBUG=interactive waybar'
 
+# TODO: Not necessary once Hot Module Replacement hits
+# See https://github.com/Aylur/ags/issues/724
 alias watch-ags='find ~/.config/ags -type f | entr -r zsh -c "ags run"'
 
