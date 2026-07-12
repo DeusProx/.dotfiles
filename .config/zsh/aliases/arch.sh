effb7f1db1acc117b36ce241ceb2538726de10a7
Gordon Lawrenz <gordon@lawrenz.email>
2026-07-13 01:17:07 +0200

    improve package and binary lookup commands: binsof pkgof

--- .config/zsh/aliases/arch.sh
+++ .config/zsh/aliases/arch.sh
@@ -1,7 +1,11 @@
 alias update-mirrors='sudo reflector --verbose -l 50 -p https --sort rate --save /etc/pacman.d/mirrorlist'
 
 # Might need to update metadata database before: `paru -Fy`
-paru-show-executables() {
-  paru -Flq $1 | grep 'bin/..*' | xargs -n1 basename
+binsof() {
+  (paru -Flq $1 || return 0) | grep 'bin/..*' | xargs -n1 basename
+}
+
+pkgof() {
+  paru -Qo "$1"
 }
 
